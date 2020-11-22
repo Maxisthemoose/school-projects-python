59f51a49a1a736330c15b3ca02426bdc1f384dbb
Maxisthemoose <oriontothemax@gmail.com>
2020-11-22 10:39:58 -0800

    FirstProject

---
+++ FirstProject.py
@@ -0,0 +1,8 @@
+for i in range(10):
+    print(
+        f"I already used python for a bit, so i think I'll do alright. {i + 1}")
+
+print("This prints seperately.")
+print("I'm used to JavaScript syntax. This feels weird.")
+print("This isn't my first python program. I've made a password generator.")
+print("This (\\n) is a line break.\nIt creates a new line.")
